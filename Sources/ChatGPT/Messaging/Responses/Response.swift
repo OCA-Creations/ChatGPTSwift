de04ccec51f4912c5f71fdd228ba0accf21b1553
MrDevel0per <104732280+MrDevel0per@users.noreply.github.com>
2023-03-01 22:29:47 -0700

    Update Response.swift

--- Sources/ChatGPT/Messaging/Responses/Response.swift
+++ Sources/ChatGPT/Messaging/Responses/Response.swift
@@ -25,4 +25,20 @@ public struct APIResponse: Codable {
     
     ///The model name used for the completion
     public var model: String
+    
+    private enum CodingKeys: String, CodingKey {
+           case id, object, created, model, usage, choices
+       }
+    
+    public init(from decoder: Decoder) throws {
+           let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.object = try container.decode(String.self, forKey: .object)
+        self.model = try container.decode(String.self, forKey: .model)
+        self.usage = try container.decode(Usage.self, forKey: .usage)
+        self.choices = try container.decode([Choice].self, forKey: .choices)
+
+           let timestamp = try container.decode(Int.self, forKey: .created)
+        self.created = Date(timeIntervalSince1970: TimeInterval(timestamp))
+       }
 }
