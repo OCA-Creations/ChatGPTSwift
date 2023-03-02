7fe82d73de279507358b66aa1aed7d36c4ed8be7
MrDevel0per <mrdeveloper081@gmail.com>
2023-03-01 22:01:17 -0700

    [Docs] Improved docs on `Header.swift` [New] Added public `init` method to `Header.swift`.

--- Sources/ChatGPT/Messaging/Header.swift
+++ Sources/ChatGPT/Messaging/Header.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  Header.swift
 //  
 //
 //  Created by Owen Cruz-Abrams on 3/1/23.
@@ -10,6 +10,12 @@ import Foundation
 ///The header for a single message.
 ///- Note: As of March 2023, this only contains the ``Role``, but the API will be updated in the future.
 public struct Header: Codable {
-    ///The role of the message sender.
+    ///The ``Role`` of the message sender.
     var role: Role
+    
+    ///Create a `Header` for a message.
+    ///- Parameter role: The ``Role`` of the sender of the message.
+    public init(role: Role) {
+        self.role = role
+    }
 }
